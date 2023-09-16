97ffad08974ca4f4466cd802cd162ddbf094de9d
Musa <musaqwabe@gmail.com>
2023-09-16 13:42:06 -0700

    Modified do_create method in the console to allow for object creation with given parameters and also added delete method to FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -8,19 +8,17 @@ class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        if kwargs:
+            for key, value in kwargs.items():
+                #  handle attribute names with underscores as spaces
+                key = key.replace('_', ' ')
+                setattr(self, key, value)
+        else:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -35,10 +33,10 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary = dict(self.__dict__)
+        dictionary['__class__'] = self.__class__.__name__
+        if 'created_at' in dictionary:
+            dictionary['created_at'] = self.created_at.isoformat()
+        if 'created_at' in dictionary:
+            dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
