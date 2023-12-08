ea5a20355b890895053530a108c19ee25519c69a
Sicelumusa <musaqwabe@gmail.com>
2023-12-08 07:21:12 -0800

    modified the script to cover all the requirements

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,22 +9,22 @@ then
 fi
 
 # Create the directories needed if not already exist
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/current
+sudo mkdir -p /data/web_static/current/releases/
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo echo -e "<html>\n <head>\n </head>\n <body>\n Holberton School\n </body>\n </html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ directory to ubuntu and the group
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve /data/web_static/current to hbnb_static
-sudo sed -i 'listen 80 default_server;/a \\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-
-# test nginx
-sudo nginx -t
+sudo sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 # restart nginx
 sudo service nginx restart
