c09c3b833043046647e1d2227c8bbfaea5d555fb
Sicelumusa <musaqwabe@gmail.com>
2023-12-10 05:32:50 -0800

    used use_sudo for put command

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,7 +16,7 @@ def do_deploy(archive_path):
         archive_name = os.path.basename(archive_path)
         archive_no = os.path.splitext(archive_name)[0]
         remote_path = '/tmp/{}'.format(archive_name)
-        put(archive_path, remote_path)
+        put(archive_path, remote_path, use_sudo=True)
         run('mkdir -p /data/web_static/releases/{}/'.format(archive_no))
         run('tar -xzf {} -C /data/web_static/releases/{}/'
             .format(remote_path, archive_no))
@@ -28,5 +28,6 @@ def do_deploy(archive_path):
         run('ln -s /data/web_static/releases/{}/ '
             '/data/web_static/current'.format(archive_no))
         return True
-    except:
+    except Exception as e:
+        print(f"Error:{e}")
         return False
