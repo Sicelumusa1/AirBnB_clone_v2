6a26153488ace5f0845489c4b07592284e91bcca
Sicelumusa <musaqwabe@gmail.com>
2023-12-25 05:49:22 -0800

    Added spaces

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,16 +24,19 @@ def hello_hbnb():
     """Displays “Hello HBNB!”"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays “HBNB”"""
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """Displays “C ” followed by the value of the text variable"""
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -25,10 +25,12 @@ def hello_hbnb():
     """Displays “Hello HBNB!”"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays “HBNB”"""
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
@@ -36,12 +38,14 @@ def c(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     """Displays “Python ” followed by the value of the text variable"""
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def numbe(n):
